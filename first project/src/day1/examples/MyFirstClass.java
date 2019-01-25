ab65b1a0a8e784bbf258b00eab86e6573bf9c5df
USER <USER@USER-PC>
2019-01-25 18:35:58 +0530

    MAA

---
+++ first project/src/day1/examples/MyFirstClass.java
@@ -0,0 +1,21 @@
+package day1.examples;
+
+public class MyFirstClass {
+/*
+ * this is my first
+ * java program.
+ * i am new to java
+ * ok
+ */
+	public static void main(String[] args) {
+		// my first statement
+		System.out.println("hello world");
+		
+		int x=10;
+		int y=20;
+		int c=x+y;
+		System.out.println(c);
+
+	}
+
+}
